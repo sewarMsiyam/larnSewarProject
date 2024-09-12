f34e846abbd6d81655045a267045cb267d4520d3
sewarMsiyam <sewarsiyam@outlook.sa>
2024-09-12 17:08:57 +0300

    .

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -29,76 +29,76 @@ export const authOptions: NextAuthOptions = {
         },
       },
     }),
-    CredentialsProvider({
-      credentials: {
-        email: { label: 'Email', type: 'email' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        const { email, password } = credentials as {
-          email: string;
-          password: string;
-        };
+  //   CredentialsProvider({
+  //     credentials: {
+  //       email: { label: 'Email', type: 'email' },
+  //       password: { label: 'Password', type: 'password' },
+  //     },
+  //     async authorize(credentials) {
+  //       const { email, password } = credentials as {
+  //         email: string;
+  //         password: string;
+  //       };
 
-        try {
-          const response = await fetch("https://sewaar.net/api/v1/students/login", {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'Accept-Language': 'ar',
-              'Authorization': `Bearer 1|5hbRkuSLBeVcqL8OtbqMrVZh8QcL4EZHPZIXFLax`,
-            },
-            body: JSON.stringify({ email, password }),
-          });
+  //       try {
+  //         const response = await fetch("https://sewaar.net/api/v1/students/login", {
+  //           method: 'POST',
+  //           headers: {
+  //             'Content-Type': 'application/json',
+  //             'Accept-Language': 'ar',
+  //             'Authorization': `Bearer 1|5hbRkuSLBeVcqL8OtbqMrVZh8QcL4EZHPZIXFLax`,
+  //           },
+  //           body: JSON.stringify({ email, password }),
+  //         });
 
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
+  //         if (!response.ok) {
+  //           throw new Error('Network response was not ok');
+  //         }
 
-          const result = await response.json();
+  //         const result = await response.json();
 
-          if (result.status === 200 && result.item) {
-            const user = result.item.student;
-            return {
-              id: user.id,
-              email: user.email,
-              name: user.name,
-            };
-          } else {
-            throw new Error(result.message || 'Sign in failed.');
-          }
-        } catch (error) {
-          console.error('Authentication error:', error);
-          return null;
-        }
-      },
-    }),
+  //         if (result.status === 200 && result.item) {
+  //           const user = result.item.student;
+  //           return {
+  //             id: user.id,
+  //             email: user.email,
+  //             name: user.name,
+  //           };
+  //         } else {
+  //           throw new Error(result.message || 'Sign in failed.');
+  //         }
+  //       } catch (error) {
+  //         console.error('Authentication error:', error);
+  //         return null;
+  //       }
+  //     },
+  //   }),
   ],
-  pages: {
-    signIn: '/login',
-  },
-  session: {
-    strategy: 'jwt',
-    maxAge: 1 * 24 * 60 * 60, // 1 day
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.email = user.email!;
-        token.name = user.name!;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (token) {
-        session.user = {
-          email: token.email as string,
-          name: token.name as string,
-        };
-      }
-      return session;
-    },
-  },
+  // pages: {
+  //   signIn: '/login',
+  // },
+  // session: {
+  //   strategy: 'jwt',
+  //   maxAge: 1 * 24 * 60 * 60, // 1 day
+  // },
+  // callbacks: {
+  //   async jwt({ token, user }) {
+  //     if (user) {
+  //       token.email = user.email!;
+  //       token.name = user.name!;
+  //     }
+  //     return token;
+  //   },
+  //   async session({ session, token }) {
+  //     if (token) {
+  //       session.user = {
+  //         email: token.email as string,
+  //         name: token.name as string,
+  //       };
+  //     }
+  //     return session;
+  //   },
+  // },
 };
 
 // Export named HTTP method handlers
