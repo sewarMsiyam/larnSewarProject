07f93a68da7c0757f7a6fd3f73b9676265307e63
sewarMsiyam <sewarsiyam@outlook.sa>
2024-10-15 08:40:57 +0300

    .

--- src/components/pages/instructorProfile/index.tsx
+++ src/components/pages/instructorProfile/index.tsx
@@ -21,7 +21,7 @@ export default function ProfileInstructor({token }: CheckoutFormProps) {
                             {/* {token} */}
                 <div className="flex flex-col justify-center items-center space-y-10 -mt-10">
                     <ImgUser token={token}  />
-                    <Tabs defaultValue="houerLib" dir="rtl" className='w-full'>
+                    <Tabs defaultValue="course" dir="rtl" className='w-full'>
                         <TabsList className='mb-5'>
                             <TabsTrigger value="course">
                                 <Image src="/profileIcon/course.svg" alt='الكورسات' width="30" height="30" />
