ea14f5b2e23841b20228dcc74fab554426c9c451
sewar siyam <99339170+sewarMsiyam@users.noreply.github.com>
2025-03-11 15:36:42 +0200

    ..

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        unoptimized: true, // فقط عند الحاجة
+      },
+};
  
 module.exports = withNextIntl(nextConfig);
